7aa49ddc85dcc54a18d9e921e4b1c832548b0d08
Luis David <cusiluis04@gmail.com>
2024-02-20 23:38:37 -0500

    Subiendo practica TwitterFollowCard

---
+++ projects/TwitterFollowCard/src/main.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import {App} from './App'
+import './index.css'
+
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+
+// Renderizar elementos
+root.render(
+ <App />
+
+)
