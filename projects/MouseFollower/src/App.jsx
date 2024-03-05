fe480cb727057659ee8154bd008ec3d57dc922d6
Luis David <cusiluis04@gmail.com>
2024-03-05 10:33:35 -0500

    Nuevo UseEffect no-cursor

--- projects/MouseFollower/src/App.jsx
+++ projects/MouseFollower/src/App.jsx
@@ -4,9 +4,10 @@ import { useState, useEffect } from 'react'
 function App() {
   //useState añade estado a los componentes
   const [enabled, setEnabled] = useState(false)
-  const [position, setPosition] = useState({x: 0 , y: 0})
+  const [position, setPosition] = useState({x: 500 , y: 400})
 
   //useEffect permite realizar efectos secundarios
+  // Efecto Pointer Move
   useEffect(() => {
     console.log('effect ', {enabled})
 
@@ -25,18 +26,29 @@ function App() {
     }
   }, [enabled])
 
+
+  useEffect(() => {
+    document.body.classList.toggle('no-cursor', enabled)
+
+    return() => {
+      document.body.classList.remove('no-cursor')
+    }
+
+  }, [enabled])
+
   return (
     <main>
       <div style={{
         position: 'absolute',
-        backgroundColor: '#09f',
+        background: 'rgb(63,94,251)',
+        background: 'radial-gradient(circle, rgba(63,94,251,1) 0%, rgba(252,70,107,1) 100%)',
         borderRadius: '50%',
         opacity: 0.8,
         pointerEvents: 'none',
         left: -20,
         top: -20,
-        width: 40,
-        height: 40,
+        width: 100,
+        height: 100,
         transform: `translate(${position.x}px, ${position.y}px)`
       }}
       />
